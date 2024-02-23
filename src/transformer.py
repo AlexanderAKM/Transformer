7b12286f21948a23322166dea24b427d333c4283
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-23 09:22:42 +0100

    Normalization and FFN done

--- src/transformer.py
+++ src/transformer.py
@@ -29,7 +29,7 @@ class InputEmbeddings(nn.Module):
     
 class PositionalEncoding(nn.Module):
     
-   def __init__(self, d_model: int, seq_len: int, dropout: float) -> None
+   def __init__(self, d_model: int, seq_len: int, dropout: float) -> None:
       super().__init__()
       self.d_model = d_model
       self.seq_len = seq_len
@@ -57,10 +57,49 @@ class PositionalEncoding(nn.Module):
       x = x + (self.pe[:, :x.shape[1], :]).requires_grad(False) #False is for not learned
       return self.dropout(x)
 
-
-# https://levelup.gitconnected.com/understanding-transformers-from-start-to-end-a-step-by-step-math-example-16d4e64e6eb1
 '''
-1. Create a small dataset of words to use for the Transformer.
+   Now Layer normalization. This stabilizes the learning process by computing the mean
+   and variance used for normalization across the features (not across the batch as in
+   batch normalization). The network can put more emphasis on one unit this way.'''
+# https://levelup.gitconnected.com/understanding-transformers-from-start-to-end-a-step-by-step-math-example-16d4e64e6eb1
+
+
+class LayerNormalization(nn.Module):
+   
+   def __init__(self, eps: float = 10**-6) -> None:
+      super().__init__()
+      self.eps = eps
+      self.alpha = nn.Parameter(torch.ones(1)) # Multiplitied
+      self.bias = nn.Parameter(torch.zeros(1)) # Additive
+
+   def forward(self, x):
+      mean = x.mean(dim = -1, keepdim=True)
+      std = x.std(dim = -1, keepdim=True)
+      return self.alpha * (x - mean) / (std + self.eps) + self.bias
+   
+'''
+Now the feed forward layer. 
+'''
+
+class FeedForwardBlock(nn.Module):
+
+   def __init__(self, d_model: int, d_ff: int, dropout: float) -> None:
+      super().__init__()
+      self.linear_1 = nn.Linear(d_model, d_ff) # W1 and b1
+      self.dropout = nn.Dropout(dropout)
+      self.linear_2 = nn.Linear(d_ff, d_model) # W2 and b2
+
+   def forward(self, x):
+      # (Batch, seq_len, d_model) --> (batch, seq_len, d_ff) --> (batch, seq_len, d_model)
+      return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+
+'''
+Now the multi-head attention'''
+   
+   
+   
+   
+'''1. Create a small dataset of words to use for the Transformer.
 2. Find the vocab size (number of unique words in the dataset)
 3. We assign a number to each word (encoding)
 4. Calculating the embedding of the sentence. The paper uses a 512-dimensional embedding vector
@@ -68,6 +107,4 @@ for each word. However, for visualization we will use six dimensions.
 The values in the vectors will be initialized randomly between 0 and 1.
 Later, they will be updated as the transformer starts understanding the meaning of the words.
 5. Positional encoding will now be done, as it is essential that the model knows
-   information about the order of the input sequence. 
-'''
-
+   information about the order of the input sequence.'''
