7cc89a8f946a58c3f7ae5c80928672a6184e1945
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-24 08:32:04 +0100

    Working on projection layer

--- src/transformer.py
+++ src/transformer.py
@@ -216,8 +216,15 @@ class Decoder(nn.Module):
          x = layer(x, encoder_output, src_mask, tgt_mask)
       return self.norm(x)
    
+class ProjectionLayer(nn.Module);
+   
+   def __init__(self, d_model: int, vocab_size: int) -> None:
+      super().__init__()
+      self.proj = nn.Linear(d_model, vocab_size)
 
-
+   def forward(self, x):
+      # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
+      
 
 
 
