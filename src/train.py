389e8b63e3b9ba6cfa8b96f020d1b3a7e7c46f46
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-24 11:19:39 +0100

    Tokenizer done

--- src/train.py
+++ src/train.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch.utils.data import Dataset, DataLoade, random_split
 
 from datasets import load_dataset
 from tokenizers import Tokenizer
@@ -9,6 +10,10 @@ from tokenizers.pre_tokenizers import Whitespace
 
 from pathlib import Path
 
+def get_all_sentences(ds, lang):
+    for item in ds:
+        yield item['translation'][lang]
+
 def get_or_build_tokenizer(config, ds, lang):
     # config['tokenizer_file'] = '../tokenizers/tokenizer_{0].json} example
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
@@ -16,3 +21,20 @@ def get_or_build_tokenizer(config, ds, lang):
         tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
         tokenizer.pre_tokenizer = Whitespace()
         trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+    return tokenizer
+
+def get_ds(config):
+    ds_raw = load_dataset('opus_books', f'{config["lang_src"]}-{config["lang_tgt"]}', split='train')
+
+    # Build tokenizers
+    tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
+    tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
+
+    # Split data and do 90-10 split training, validation
+    train_ds_size = int(0.9 * len(ds_raw))
+    val_ds_size = len(ds_raw) - train_ds_size
+    train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
