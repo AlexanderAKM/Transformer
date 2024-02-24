d8f4506e50eb97a3d1f39af9404e00a357cc3383
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-24 08:39:10 +0100

    All blocks done

--- src/transformer.py
+++ src/transformer.py
@@ -224,7 +224,33 @@ class ProjectionLayer(nn.Module);
 
    def forward(self, x):
       # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
-      
+      return torch.log_softmax(self.proj(x), dim = -1)
+   
+class Transformer(nn.Module):
+   def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbeddings, tgt_embed: InputEmbeddings, src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
+      super().__init__()
+      self.encoder = encoder
+      self.decoder = decoder
+      self.src_embed = src_embed
+      self.tgt_embed = tgt_embed
+      self.src_pos = src_pos
+      self.tgt_pos = tgt_pos
+      self.projection_layer = projection_layer
+
+   def encode(self, src, src_mask):
+      src = self.src_embed(src)
+      self = self.src_pos(src)
+      return self.encoder(src, src_mask)
+   
+   def decode(self, encoder_output, src_mask, tgt, tgt_mask):
+      tgt = self.tgt_embed(tgt)
+      tgt = self.tgt_pos(tgt)
+      return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+   
+   def project(self, x):
+      return self.project_layer(x)
+   
+   
 
 
 
