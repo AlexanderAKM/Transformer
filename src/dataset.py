c9b6a2b8de275b30221a3f7c92a78402b9acc399
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-24 13:56:06 +0100

    Training loop pretty much done

--- src/dataset.py
+++ src/dataset.py
@@ -1,20 +1,22 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset
+from typing import Any
 
 class BilingualDataset(Dataset):
 
     def __init__(self, ds, tokenizer_src, tokenizer_tgt, src_lang, tgt_lang, seq_len) -> None:
         super().__init__()
+        self.seq_len = seq_len
         self.ds = ds
         self.tokenizer_src = tokenizer_src
         self.tokenizer_tgt = tokenizer_tgt
         self.src_lang = src_lang
         self.tgt_lang = tgt_lang
 
-        self.sos_token = torch.Tensor([tokenizer_src.token_to_id(['[SOS]'])], dtype = torch.int64)
-        self.eos_token = torch.Tensor([tokenizer_src.token_to_id(['[EOS]'])], dtype = torch.int64)
-        self.pad_token = torch.Tensor([tokenizer_src.token_to_id(['[PAD]'])], dtype = torch.int64)
+        self.sos_token = torch.tensor([tokenizer_src.token_to_id('[SOS]')], dtype = torch.int64)
+        self.eos_token = torch.tensor([tokenizer_src.token_to_id('[EOS]')], dtype = torch.int64)
+        self.pad_token = torch.tensor([tokenizer_src.token_to_id('[PAD]')], dtype = torch.int64)
 
     def __len__(self):
         return len(self.ds)
--- src/model.py
+++ src/model.py
@@ -14,7 +14,7 @@ import math
 class InputEmbeddings(nn.Module):
     
    def __init__(self, d_model: int, vocab_size: int):
-        super().__init()
+        super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
         self.embedding = nn.Embedding(vocab_size, d_model)
@@ -35,7 +35,7 @@ class PositionalEncoding(nn.Module):
       self.seq_len = seq_len
       self.dropout = nn.Dropout(dropout)
 
-      # Create a matrix of shape (seq_len, d_model)
+      # Create a matrix of shape (seq_len, d_model)   
       pe = torch.zeros(seq_len, d_model)
       '''
       For each word we create a 512-dimensional vector, where for the even
@@ -46,7 +46,7 @@ class PositionalEncoding(nn.Module):
       div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
       # Apply the sin to even positions
       pe[:, 0::2] = torch.sin(position * div_term)
-      pe[:, 1::2] = torch.co(position * div_term)
+      pe[:, 1::2] = torch.cos(position * div_term)
 
       pe = pe.unsqueeze(0) # tensor of (1, seq_len, d_model)
 
