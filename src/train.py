5499f3fe363ed3eee16aed7915a21b4532f983d7
AlexanderAKM <a.k.muller.2@student.rug.nl>
2024-02-24 11:12:48 +0100

    Starting the tokenizer

---
+++ src/train.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import WordLevel
+from tokenizers.trainers import WordLevelTrainer
+from tokenizers.pre_tokenizers import Whitespace
+
+from pathlib import Path
+
+def get_or_build_tokenizer(config, ds, lang):
+    # config['tokenizer_file'] = '../tokenizers/tokenizer_{0].json} example
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(WordLevel(unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = Whitespace()
+        trainer = WordLevelTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], min_frequency=2)
